843636039104f9b539f14d243290ea9a28790c97
TANG Clement <clement.tang@iutseine.u-paris.domain>
2024-12-19 11:50:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if num == 1:
     print(num, "is not a prime number")
 elif num > 1:
     # check for factors
+    #Boucle
     for i in range(2, num):
         if (num % i) == 0:
             # if factor is found, set flag to True
